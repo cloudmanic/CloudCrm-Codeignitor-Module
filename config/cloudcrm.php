bd69c1447b92e2cff91ae123de9dc6c2fe001f89
Cloudmanic Labs, LLC <github@cloudmanic.com>
2010-08-14 16:19:02 -0700

    Made it super generic

--- config/cloudcrm.php
+++ config/cloudcrm.php
@@ -6,11 +6,22 @@
 |
 |
 */
-$config['ccrmdbprefix'] = 'elevationfit_';
-$config['ccrmdomain'] = 'elevationfit.com';
-$config['ccrmconfigspacer'] = '_';
+$url = str_ireplace('https://', '', base_url());
+$url = str_ireplace('/', '', $url);
+$url = explode('.', $url);
+
+$config['ccrmdbprefix'] = $url[1] . '_';
+$config['ccrmdomain'] = $url[1] . '.' . $url[2];
 $config['ccrmtheme'] = 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.4/themes/smoothness/jquery-ui.css';
 $config['ccrmignoreprefix'] = array('www', 'blog', 'company', 'beta', 'alpha', 'charlie');
-$config['ccrmUsers'][]	= array('email' => 'spicer@elevationfit.com', 'domain' => 'spicer.elevationfit.com');
-$config['ccrmUsers'][]	= array('email' => 'spicer@elevationfit.com', 'domain' => 'dev.elevationfit.com');
+$config['ccrmUsers'][]	= array('email' => 'spicer@elevationfit.com', 'domain' => 'spicer.' . $config['ccrmdomain']);
+$config['ccrmUsers'][]	= array('email' => 'spicer@elevationfit.com', 'domain' => 'dev.' . $config['ccrmdomain']);
+$config['ccrmUsers'][]	= array('email' => 'spicer@cloudmanic.com', 'domain' => 'spicer.' . $config['ccrmdomain']);
+$config['ccrmUsers'][]	= array('email' => 'spicer@cloudmanic.com', 'domain' => 'dev.' . $config['ccrmdomain']);
+
+// Handle older config schemas
+if($config['ccrmdomain'] == 'elevationfit.com') 
+	$config['ccrmconfigspacer'] = '_';
+else
+	$config['ccrmconfigspacer'] = '';
 ?>
